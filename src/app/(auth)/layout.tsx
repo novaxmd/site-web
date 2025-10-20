264a36b420185fa8abaf02ef4036ee6c90b67f7f
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-10-21 01:00:54 +0300

    Create layout.tsx

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+// This Layout page is build for to provide auth pages a common structure whithout repating code
+
+import AuthHeader from "@/components/auth-components/AuthHeader";
+import ThemeProvider from "@/context/ThemeProvider";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <>
+      {/* <AuthProvider> */}
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <AuthHeader />
+        <main className="w-full" style={{ height: "calc(100vh - 57px)" }}>
+          {children}
+        </main>
+      </ThemeProvider>
+      {/* </AuthProvider> */}
+    </>
+  );
+      }
+    
