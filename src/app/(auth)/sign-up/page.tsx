dba9528273fee1115aabb9fdf919c60d370e8db6
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-10-21 01:02:56 +0300

    Create page.tsx

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,239 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils/utils";
+import TypographyH3 from "@/components/typography/TypographyH3";
+import TypographyMuted from "@/components/typography/TypographyMuted";
+import TypographySmall from "@/components/typography/TypographySmall";
+import Link from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
+import PolicyMsg from "@/components/auth-components/PolicyMsg";
+import SeparatorAuth from "@/components/auth-components/SeparatorAuth";
+import GoogleAuthButton from "@/components/auth-components/GoogleAuthButton";
+import { signIn } from "next-auth/react";
+import { Suspense, useEffect, useState } from "react";
+import ErrorAlert from "@/components/global/form/ErrorAlert";
+import { formSchemaSignUp } from "@/lib/zod/index.zodSchema";
+import axios from "axios";
+import { toast as reactToastify } from "react-toastify";
+import toast from "react-hot-toast";
+import ButtonSpinner from "@/components/global/spinner/ButtonSpinner";
+import { clientError } from "@/lib/utils/error/errorExtractor";
+import AuthWrapper from "@/components/auth-components/AuthWrapper";
+import OtpVerificationCom from "@/components/global/auth/OtpVerification";
+
+// React conponent startss here
+const SignUp = () => {
+  const rout = useRouter();
+  const [isError, setIsError] = useState<string>("");
+  const [signupStatus, setSignupStatus] = useState<
+    "started" | "creating" | "redirecting"
+  >("started");
+
+  const searchParams = useSearchParams();
+  const isVerify = searchParams.get("verify");
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchemaSignUp>) {
+    setSignupStatus("creating");
+
+    toast
+      .promise(axios.post("/api/user/signup", values), {
+        loading: "Signing you up...",
+        success: "Signed up successfully!",
+        error: "Failed to sign up. Please try again.",
+      })
+      .then((res) => {
+        const {
+          status,
+          data: { message, user },
+        } = res;
+
+        if (status === 201) {
+          const payload = user;
+
+          signIn("credentials", {
+            redirect: false,
+            ...payload,
+            ...payload.avatar,
+          })
+            .then(() => {
+              toast.success("Redirecting to verification page");
+              setSignupStatus("redirecting");
+            })
+            .catch((error) => {
+              setSignupStatus("started");
+              const errorMessage = clientError(error);
+
+              setIsError(errorMessage); // Set the extracted message as the error state
+            });
+
+          return;
+        }
+        setSignupStatus("started");
+      })
+      .catch((error) => {
+        setSignupStatus("started");
+        const errorMessage = clientError(error);
+
+        setIsError(errorMessage); // Set the extracted message as the error state
+      });
+  }
+
+  useEffect(() => {
+    if (signupStatus === "redirecting") {
+      toast.success("Redirection to verification page");
+      rout.push("/sign-up?verify=true");
+      setSignupStatus("started");
+    }
+  }, [signupStatus]);
+
+  const form = useForm<z.infer<typeof formSchemaSignUp>>({
+    resolver: zodResolver(formSchemaSignUp),
+    defaultValues: {
+      fullname: "",
+      email: "",
+      phone: "",
+      password: "",
+    },
+  });
+
+  if (isVerify) {
+    return (
+      <OtpVerificationCom
+        title="Unlock the door!"
+        subTitle="We've sent a key to your phone (virtually, of course). Enter it here to open your account."
+      />
+    );
+  }
+
+  return (
+    <AuthWrapper
+      title="Ready to take the next step?"
+      subTitle="Sign up and take control."
+    >
+      <div className="text-center mb-3">
+        <TypographyH3>It all starts with you!</TypographyH3>
+        <TypographyMuted>
+          Enter your name, email, phone number and password to become part of
+          our growing community.
+        </TypographyMuted>
+      </div>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+          {/* name field */}
+          <FormField
+            control={form.control}
+            name="fullname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Md Faizan" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* email field */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="faizan@gmail.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* phone field */}
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input placeholder="1234567890" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* password field */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="F$%$N6573n#6y" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {isError && <ErrorAlert isError={isError} setIsError={setIsError} />}
+          {signupStatus === "started" ? (
+            <Button
+              type="submit"
+              className={cn("disabled:bg-gray-500 w-full mt-4 py-1")}
+            >
+              Next
+            </Button>
+          ) : signupStatus === "creating" ? (
+            <div className="flex items-center justify-center gap-2 py-1">
+              <ButtonSpinner className="w-5 h-5" /> Creating user!
+            </div>
+          ) : (
+            <div className="flex items-center justify-center gap-2 py-1">
+              {" "}
+              <ButtonSpinner className="w-5 h-5" /> Redirecting to verification
+              page!
+            </div>
+          )}
+          <div>
+            <TypographySmall>
+              Already have account?{" "}
+              <Link href="/sign-in" className="text-blue-600">
+                Sign in here
+              </Link>
+            </TypographySmall>
+          </div>
+        </form>
+      </Form>
+      {/* <SeparatorAuth />
+      <GoogleAuthButton /> */}
+      <PolicyMsg />
+    </AuthWrapper>
+  );
+};
+
+const SignUpPage = () => {
+  return (
+    <Suspense>
+      <SignUp />
+    </Suspense>
+  );
+};
+
+export default SignUpPage;
